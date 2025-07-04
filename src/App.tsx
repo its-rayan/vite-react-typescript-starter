7391fc0f6561c4112521727db3ee8601c8c20fd1
its-rayan <rayanmabdirahman@gmail.com>
2025-07-04 23:52:46 +0100

    add wouter for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { Button } from "./components/ui/button";
+import { Route, Switch } from "wouter";
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
-      <Button>Click me</Button>
+      {/* 
+      Routes below are matched exclusively -
+      the first matched route gets rendered
+    */}
+      <Switch>
+        <Route path="/">
+          <h1>Welcome to the homepage!</h1>
+        </Route>
+
+        <Route path="/users/:name">
+          {(params) => <>Hello, {params.name}!</>}
+        </Route>
+
+        {/* Default route in a switch */}
+        <Route>404: No such page!</Route>
+      </Switch>
     </>
   );
 }
